2b6f2d28e0adb3d21b4c8f53ce4d81fea50d2010
Rasie1 <rasielll@gmail.com>
2015-11-04 20:16:39 +0300

    Recolorer fixes + idris + a lot of stuff

--- Packages/User/AutoRecolorer.py
+++ Packages/User/AutoRecolorer.py
@@ -37,9 +37,12 @@ def change(colors):
     for color in colors:
         color.replaceWholeText(rgb_to_hex(rgb))
     return
+    
 
 class AutoRecolorer(sublime_plugin.TextCommand):
     def run(self, edit):
+        defaultScheme = sublime.packages_path() + "/User/VSDefault.tmTheme"
+        currentScheme = sublime.packages_path() + "/User/VS.tmTheme"
         path = currentScheme
         dom = parse(path)
         array = dom.childNodes[2].childNodes[1].childNodes[11]
@@ -65,11 +68,70 @@ class AutoRecolorer(sublime_plugin.TextCommand):
 
 class AutoRecolorerReset(sublime_plugin.TextCommand):
     def run(self, edit):
+        path = sublime.packages_path()
+        defaultScheme = path + "/User/VSDefault.tmTheme"
         shutil.copyfile(defaultScheme, currentScheme)
 
+def toggle_night(colors, daycolor, nightcolor, is_night):
+    rgb = hex_to_rgb(colors[0].nodeValue)
+    if (is_night):
+        day_color_hex = rgb_to_hex(daycolor)
+        for color in colors:
+            color.replaceWholeText(day_color_hex)
+    else:
+        night_color_hex = rgb_to_hex(nightcolor)
+        for color in colors:
+            color.replaceWholeText(night_color_hex)
+    return
+
 class AutoRecolorerNightModeToggle(sublime_plugin.TextCommand):
+
     def run(self, edit):
+
+        currentScheme = sublime.packages_path() + "/User/VS.tmTheme"
         path = currentScheme
-        #todo - make a night mode toggle
-        #dom = parse(path)
-        #array = dom.childNodes[2].childNodes[1].childNodes[11]
+
+        dom = parse(path)
+        array = dom.childNodes[2].childNodes[1].childNodes[11]
+
+        mode = array.childNodes[1].childNodes[3].childNodes[3].childNodes[0].nodeValue == "#111111"
+
+        #bg
+        toggle_night([array.childNodes[1].childNodes[3].childNodes[3].childNodes[0]], (249, 249, 249), (17, 17, 17), mode)
+        #caret
+        toggle_night([array.childNodes[1].childNodes[3].childNodes[7].childNodes[0]], (17, 17, 17), (220, 220, 220), mode)
+        #fg
+        toggle_night([array.childNodes[1].childNodes[3].childNodes[11].childNodes[0]], (17, 17, 17), (221, 221, 221), mode)
+        #highlight
+        toggle_night([array.childNodes[1].childNodes[3].childNodes[19].childNodes[0]], (255, 255, 255), (15, 15, 15), mode)
+
+        #variable
+        toggle_night ([array.childNodes[5].childNodes[11].childNodes[7].childNodes[0]], (17, 17, 17), (221, 221, 221), mode)
+        #comparison
+        toggle_night ([array.childNodes[9].childNodes[11].childNodes[7].childNodes[0]], (17, 17, 17), (221, 221, 221), mode)
+        #assignment
+        toggle_night ([array.childNodes[11].childNodes[11].childNodes[7].childNodes[0]], (17, 17, 17), (221, 221, 221), mode)
+        #arithmetic
+        toggle_night ([array.childNodes[13].childNodes[11].childNodes[7].childNodes[0]], (17, 17, 17), (221, 221, 221), mode)
+        #fun
+        toggle_night ([array.childNodes[31].childNodes[11].childNodes[7].childNodes[0]], (17, 17, 17), (221, 221, 221), mode)
+        #lib fun
+        toggle_night ([array.childNodes[49].childNodes[11].childNodes[7].childNodes[0]], (17, 17, 17), (221, 221, 221), mode)
+        #lib var
+        toggle_night ([array.childNodes[55].childNodes[11].childNodes[7].childNodes[0]], (17, 17, 17), (221, 221, 221), mode)
+
+        #tab bg
+
+        #tab fg
+
+        #sidebar tab bg
+
+        #sidebar tab fg
+
+        #cmd bg
+
+        #cmd fg
+
+        f = open(path, 'w')
+        dom.writexml(f)
+        f.close()
